12ef9c9f6f452249ff3ad7dc500034bde8c8608a
eduuuards <98739204+eduuuards@users.noreply.github.com>
2024-02-24 13:33:43 +0000

    Pievienota daļēji strādājoša funkcija redzet pilsetas laikapstaklus kas tiek nolasiti no API

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const displejs = document.getElementById("displejs");
-
 function rakstitDisp(input){
   displejs.value += input;
 }
@@ -9,10 +8,73 @@ function notiritDisp(){
 }
 
 function aprekinat(){
+  
   try{
      displejs.value = eval(displejs.value);
+    
   }
   catch(error){
     displejs.value = "Kļūda";
   }
 }
+function paraditInfo() {
+    var container = document.getElementById(".container");
+    if (container.style.display === "none") {
+        container.style.display = "block";
+    } else {
+        container.style.display = "none";
+    }
+}
+
+
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
+
+    const APIKey = '3bc28c6d252886673f6a29c24af932e6';
+    const city = document.querySelector('.search-box input').value;
+
+    if (city === '')
+        return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(json => {
+
+            if (json.cod === '404') {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
+
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
+
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const mitrums = document.querySelector('.weather-details .mitrums span');
+            const vejs = document.querySelector('.weather-details .vejs span');
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            mitrums.innerHTML = `${json.main.humidity}%`;
+            vejs.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '590px';
+
+
+        });
+
+
+});
